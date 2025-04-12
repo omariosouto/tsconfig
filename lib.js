91a8adcd4429775625e6e18b574ec35cfb45f3e3
Mario Souto <13791385+omariosouto@users.noreply.github.com>
2025-04-12 13:09:40 -0300

    update

--- lib.js
+++ lib.js
@@ -317,7 +317,7 @@ ${PACKAGE_JSON.version}
 
         // Check if PR has labels
         if (prInfo.labels.length === 0) {
-          await this.addCommentToPR("No labels found, please add one of major|minor|patch labels");
+          await this.addCommentToPR("No labels found, please add one of `major`,`minor` or `patch` labels");
           throw new Error("No labels found");
         }
         // Check if PR has valid labels
